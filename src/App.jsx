64e8eaabb32d1b42288be8761ccd52c04205780d
unknown <ram.chow93@gmail.com>
2025-06-25 18:53:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+    return (
+        <div>
+            <h1 className={"text-4xl text-green-800 text-center"}>Hello World !!!!!!!!</h1>
+        </div>
+    )
+}
+
+export default App
